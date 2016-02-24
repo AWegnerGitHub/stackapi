44362d02498ce04c680a9202a0770c4e81a2a6b2
AWegnerGitHub <andy@andrewwegner.com>
2016-02-24 14:22:43 -0600

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     keywords='stackexchange',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'test']),
-    version='0.1.4',
+    version='0.1.5',
     install_requires=['requests'],
     classifiers=[
         'Development Status :: 4 - Beta',
